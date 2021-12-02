0d5e84e879b6555046119c68165af6fbef8229ba
Balumba <cvdfer1234@mail.ru>
2021-12-02 17:37:03 +0300

    first commit

---
+++ app/src/main/java/com/example/gittt/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gittt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click (View view){
+        String name = "0";
+
+    }
+}g
